f2bb1278bd96dcd61bdd4c4199a089660d9b5cf9
Michael Shick <m@shick.us>
2016-08-15 11:47:34 -0700

    fix rollup config for pre node6

--- templates/rollup.config.js
+++ templates/rollup.config.js
@@ -2,7 +2,8 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
-const {NODE_ENV} = process.env;
+
+const NODE_ENV = process.env.NODE_ENV;
 
 export default {
   plugins: [
